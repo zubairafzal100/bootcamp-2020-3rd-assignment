376d94892fa499f7ccfaaffbc0ece6124070ff86
Zubair Afzal <53823564+zubairafzal100@users.noreply.github.com>
2020-06-13 19:17:44 +0500

    Create new component info.js and add into app.js file

---
+++ .github/info.js
@@ -0,0 +1,13 @@
+import React from'react';
+
+function MyInfo (props) {
+    return(
+        <div>
+            <h1>I'm {props.myName}</h1>
+            <h1>I'm {props.myEdu}</h1>
+            <h1>Thnkas Sir Zia Khan and Team for creating this amazing opportunity for us</h1>
+        </div>
+    )
+}
+
+export default MyInfo;
